bf27662704b6c3f8cf95e25259d53ef7880277b0
batti251 <bastibuenz25@gmail.com>
2025-05-15 22:52:40 +0200

    created index page (login), outsourced desktop_template.css & login.css , added guest-button function to desktop_template.html

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,8 @@ function loadContent(page) {
   main.setAttribute("w3-include-html", page);
   w3.includeHTML();
 }
+
+
+function guestLogin(){
+  location.href = '/templates/desktop_template.html'
+}
