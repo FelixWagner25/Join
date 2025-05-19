12cd98d625863d3d79a8224a56cc76218b12a4a3
Felix Wagner <post@felixwagner.eu>
2025-05-19 12:05:07 +0200

    added add contact input screen html structure in contacts.html

---
+++ scripts/contacts.js
@@ -0,0 +1,11 @@
+/**
+ * This function is used to show the add contact input screen
+ *
+ */
+function showAddContactScreen() {}
+
+/**
+ * This function closes any overlay at the contacts page.
+ *
+ */
+function closeOverlay() {}
