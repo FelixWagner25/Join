6d442bf92672e788c575fc6a41ea60fd0e812e99
batti251 <bastibuenz25@gmail.com>
2025-05-22 21:39:36 +0200

    added first signup logic + credential preperation in JSON-format for posting login-data

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let database = 'https://join-461-default-rtdb.europe-west1.firebasedatabase.app/'
-
+const database = 'https://join-461-default-rtdb.europe-west1.firebasedatabase.app/'
 
 /**
  * Function to log in as guest
@@ -9,6 +8,10 @@ function guestLogin() {
   location.href = "/templates/desktop_template.html";
 }
 
+/**
+ * Function to forward user to signup page
+ *
+ */
 function signupPage() {
   location.href = "assets/html/signup.html";
 }
@@ -17,4 +20,58 @@ function stopEventPropagation(event) {
   event.stopPropagation();
 }
 
+/**
+ * Function to validate users credentials after submit
+ * 
+ * @param {object} event event-object to stop page-reload after submit
+ */
+function signupFormValidation(event) {
+    event.preventDefault();
+    let userInput = document.getElementsByTagName('input')
+    let mail = document.getElementById('email')
+    if (userInput[2].value === userInput[3].value &&
+      mail.value.includes('.'))
+      /* checkbox validation */
+     {
+      console.log("alles richtig");
+      getNewUserInformation()
+    } else {
+      /* alert if validation is faulty  */
+    console.log("etwas falsch");
+  }
+}
 
+/**
+ * Function to create JSON-object
+ * 
+ */
+function getNewUserInformation(){
+  let userInput = document.getElementsByTagName('input')
+  let userCredential = {}
+  for (let index = 0; index < userInput.length; index++) {
+      let key = userInput[index].name
+      let value = userInput[index].value
+      userCredential[key] = value;
+  }
+  console.log(userCredential);
+}
+
+/**
+ * Function to send JSON to firebase-server
+ * 
+ * @param {*} path storage path on firebase-server
+ * @param {JSON} data user-credentials as JSON
+ */
+async function submitNewUser(path="" , data={}) {
+  let response = await fetch(database + path + ".json",
+    {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(data)
+    }
+   );
+
+  
+}
