435958a0eee1420316a87feac2c6ad258932fa9d
Felix Wagner <post@felixwagner.eu>
2025-06-08 13:58:37 +0200

    fix: fix error in add contacts form due to name collision of two addNewContact() functions

--- script.js
+++ script.js
@@ -45,15 +45,18 @@ function checkInput() {
 /**
  * Function to reset error-messages on login & signup page
  *
- * 
+ *
  */
 function resetErrorMessage() {
-  let error = document.getElementsByTagName('input')
-  let message = document.getElementsByClassName('validation');
-      [...error].forEach((element) => {element.parentElement.classList.remove("error-border")});
-      [...message].forEach((message) => {message.classList.add("d-none")})
-  }
-
+  let error = document.getElementsByTagName("input");
+  let message = document.getElementsByClassName("validation");
+  [...error].forEach((element) => {
+    element.parentElement.classList.remove("error-border");
+  });
+  [...message].forEach((message) => {
+    message.classList.add("d-none");
+  });
+}
 
 /**
  * Function to validate users credentials after button submit
@@ -63,11 +66,12 @@ function resetErrorMessage() {
  */
 function signupFormValidation(event) {
   event.preventDefault(); /* preventDefault nur nutzen, wenn Validierungsfehler */
-  const regex =/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  const regex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   let userInput = document.getElementsByTagName("input");
   let mail = document.getElementById("add-contact-input-email");
   console.log(mail);
-  
+
   let passwordError = document.getElementById("password-error");
   let mailError = document.getElementById("email-error");
   if (userInput[2].value !== userInput[3].value) {
@@ -97,7 +101,6 @@ function showPassword(x) {
   }
 }
 
-
 /**
  * Function to create JSON-object (signup - user credential), if checkMailRedundancy is false
  *
@@ -113,70 +116,69 @@ function getNewUserInformation() {
   checkMailRedundancy(userCredential);
 }
 
-
 /**
  * This function validates, if the mail during sign-up-process is already used in the database
- * 
- * @param {object} credentials the sign-up credentials 
+ *
+ * @param {object} credentials the sign-up credentials
  */
-async function checkMailRedundancy(credentials){
+async function checkMailRedundancy(credentials) {
   let mailError = document.getElementById("email-redundancy-error");
   let response = await fetch(database + "/user" + ".json");
   let responseRef = await response.json();
-    if (responseRef === null){
-      postJSON("user", credentials);
-      showMessage(credentials);
-    return}
+  if (responseRef === null) {
+    postJSON("user", credentials);
+    showMessage(credentials);
+    return;
+  }
   let mailValue = Object.values(responseRef);
-  let newMail = mailValue.map((i) => {return i.email})
+  let newMail = mailValue.map((i) => {
+    return i.email;
+  });
   if (!newMail.includes(credentials.email)) {
-      postJSON("user", credentials);
-      showMessage(credentials);
+    postJSON("user", credentials);
+    showMessage(credentials);
   } else {
     mailError.classList.remove("d-none");
     mailError.previousElementSibling.classList.add("error-border");
+  }
 }
-}
-
 
 /**
  * This Function gives the User feedback, if signup was successful.
- * Also triggers to add a new contact 
- * 
+ * Also triggers to add a new contact
+ *
  * @param {object} credentials object to
  */
 function showMessage(credentials) {
-  addNewContact(credentials)
-  let messageBox = document.querySelector('.signup-message')
-  let newBlur = document.querySelector('.background-fade')
-  let signup = document.querySelector('.signup')
-  messageBox.classList.remove('d-none')
-  messageBox.classList.add('d-flex-row-c-c')
-  newBlur.style.backgroundColor = "rgb(0, 0, 0, 0.10)"
+  addNewContactOnSignup(credentials);
+  let messageBox = document.querySelector(".signup-message");
+  let newBlur = document.querySelector(".background-fade");
+  let signup = document.querySelector(".signup");
+  messageBox.classList.remove("d-none");
+  messageBox.classList.add("d-flex-row-c-c");
+  newBlur.style.backgroundColor = "rgb(0, 0, 0, 0.10)";
   signup.style.zIndex = "-1";
   setTimeout(() => {
-    location.href = "/index.html"
+    location.href = "/index.html";
   }, 1800);
 }
 
-
 /**
  * This function creates a contact-object from users signup and posts it into /contacts-path
- * 
- * @param {object} contactData dedicated information for contact-list 
+ *
+ * @param {object} contactData dedicated information for contact-list
  */
-function addNewContact(contactData) {
-  let contactObj = {}
+function addNewContactOnSignup(contactData) {
+  let contactObj = {};
   console.log(contactData);
   contactObj = {
-    email: contactData.email , 
+    email: contactData.email,
     name: contactData.name,
-    phone: ""
-  }
+    phone: "",
+  };
   console.log(contactObj);
-  postJSON("contacts" , contactObj)
+  postJSON("contacts", contactObj);
 }
-
 
 /**
  * Function to send JSON to firebase-server
@@ -194,10 +196,9 @@ async function postJSON(path = "", data = {}) {
   });
 }
 
-
 /**
  * This function gets all users-credentials from database and triggers credential-check-function
- * 
+ *
  * @param {string} path path of the database
  */
 async function userLogin(path = "user") {
@@ -208,27 +209,30 @@ async function userLogin(path = "user") {
     },
   });
   let responseRef = await response.json();
-  checkLogInCredentials(responseRef)
+  checkLogInCredentials(responseRef);
 }
 
 /**
  * This function checks, if login-credentials are valid to credentials from database
- * 
+ *
  * @param {object} responseRef all user credentials from the database
  */
 function checkLogInCredentials(responseRef) {
-  let x = Object.values(responseRef)
-  let loginError = document.getElementById('password-error')
-  let loginInput = document.getElementsByTagName('input')
-  let credentialsMerge = x.map((i)=> {return i.email + i.password});
-  if(credentialsMerge.includes(loginInput[0].value + loginInput[1].value)) {
-    location.href = "/templates/desktop_template.html"
+  let x = Object.values(responseRef);
+  let loginError = document.getElementById("password-error");
+  let loginInput = document.getElementsByTagName("input");
+  let credentialsMerge = x.map((i) => {
+    return i.email + i.password;
+  });
+  if (credentialsMerge.includes(loginInput[0].value + loginInput[1].value)) {
+    location.href = "/templates/desktop_template.html";
   } else {
     loginError.classList.remove("d-none");
-    [...loginInput].forEach((input) => {input.parentElement.classList.add("error-border");
-    })
-  };
+    [...loginInput].forEach((input) => {
+      input.parentElement.classList.add("error-border");
+    });
   }
+}
 
 /**
  * Function to get database element from firebase server as JSON
