9695e0b74f3d88ae5560eed201d2c7441581050e
Felix Wagner <post@felixwagner.eu>
2025-06-21 16:59:45 +0200

    refactor: rename and refactor task card css classes

--- templates/board_templates.js
+++ templates/board_templates.js
@@ -1,19 +1,19 @@
 function getBoardCardTemplate(indexTask) {
   return `
-    <div class="board-card-wrap">
-          <div class="board-card d-flex-column">
-            <div class="task-category ${getTaskCategoryClass(
+    <div class="task-card-wrap">
+          <div class="task-card d-flex-column">
+            <div class="task-category font-Inter-400-16px ${getTaskCategoryClass(
               tasksArray[indexTask][1].category
             )} d-flex-row-c-c">${tasksArray[indexTask][1].category}</div>
-            <div class="board-card-content d-flex">
-                <div class="task-headline">${
+            <div class="task-description-wrap d-flex">
+                <div class="task-headline font-Inter-700-16px">${
                   tasksArray[indexTask][1].title
                 }</div>
-                <div class="task-description">${
+                <div class="task-description font-Inter-400-16px">${
                   tasksArray[indexTask][1].description
                 }</div>
-                <div class="users-wrap">
-                    <div class="users-icon-container d-flex">
+                <div class="d-flex-c-sb">
+                    <div class="d-flex" id="task-contacts">
                     </div>
                     <img src=${getTaskPriorityIconSrc(
                       tasksArray[indexTask][1].priority
@@ -24,3 +24,5 @@ function getBoardCardTemplate(indexTask) {
     </div>
     `;
 }
+
+function getTaskCardContactsTemplate(indexTask) {}
