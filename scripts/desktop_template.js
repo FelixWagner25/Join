0c3210d66e31abdea3a87685c63f92fdc2da02ca
Sebastian Bünz <bastibuenz25@gmail.com>
2025-07-29 22:01:58 +0200

    Desktop_Template: Code review/refactor and added JSdoc

--- scripts/desktop_template.js
+++ scripts/desktop_template.js
@@ -1,30 +1,17 @@
 /**
- * Function to load summary.html on initiation
+ * Function to load correct header and nav by initiation
  *
  */
 async function init() {
   w3.includeHTML(() => {
-    userInitials(), setBackgroundColor();
+    userInitials(), setNavBackgroundColor();
   });
 }
 
-function toggleDropdown() {
-  let dropdown = document.querySelector(".dropdown-menu-container");
-  dropdown.classList.toggle("d-none");
-}
-
-document.addEventListener("click", function (event) {
-  const dropdownWrap = document.querySelector(".dropdown-menu-container");
-  const profileBadge = document.querySelector(".profile-ellipse");
-  if (
-    dropdownWrap &&
-    !dropdownWrap.contains(event.target) &&
-    !profileBadge.contains(event.target)
-  ) {
-    dropdownWrap.classList.add("d-none");
-  }
-});
-
+/**
+ * This Function renders correct user names Initials into the users-icon-menu
+ * 
+ */
 function userInitials() {
   let initials = document.getElementById("profile-initials");
   let sessionInititals = sessionStorage.getItem("initials") || "G";
@@ -32,6 +19,10 @@ function userInitials() {
   initials.innerHTML = sessionInititals;
 }
 
+/**
+ * This Function renders navbar, according to the users login status
+ * 
+ */
 function checkLoginStatus() {
   let previousLink = document.referrer;
   let visitor = sessionStorage.getItem("user");
@@ -47,13 +38,23 @@ function checkLoginStatus() {
   checkWorkaround(previousLink, visitor);
 }
 
+/**
+ * This function checks, if user tries to get access to board without using the login page
+ * 
+ * @param {String}} previousLink sets the previous active link from the tab
+ * @param {String} visitor indicator for active user/ valid Guest or unallowed access
+ */
 function checkWorkaround(previousLink, visitor) {
   if (visitor == null && !previousLink) {
     location.href = "/index.html";
   }
 }
 
-function setBackgroundColor() {
+/**
+ * This Function highlights current active board-tab
+ * 
+ */
+function setNavBackgroundColor() {
   let activePage = window.location.pathname;
   let links = document.getElementsByClassName("nav-link");
   let activeLink = [...links].filter((l) => l.href.includes(activePage));
@@ -61,6 +62,35 @@ function setBackgroundColor() {
   activeLink.forEach((e) => e.classList.add("active"));
 }
 
+/**
+ * Deletes SessionStorage
+ * 
+ */
 function deleteSessionStorage() {
   sessionStorage.clear();
 }
+
+/**
+ * This function toggles the dropdown menu from the users-icon-menu in the header
+ * 
+ */
+function toggleDropdown() {
+  let dropdown = document.querySelector(".dropdown-menu-container");
+  dropdown.classList.toggle("d-none");
+}
+
+/**
+ * This function removes opened dropdown menu, when clicked outside the menu
+ * 
+ */
+document.addEventListener("click", function (event) {
+  const dropdownWrap = document.querySelector(".dropdown-menu-container");
+  const profileBadge = document.querySelector(".profile-ellipse");
+  if (
+    dropdownWrap &&
+    !dropdownWrap.contains(event.target) &&
+    !profileBadge.contains(event.target)
+  ) {
+    dropdownWrap.classList.add("d-none");
+  }
+});
