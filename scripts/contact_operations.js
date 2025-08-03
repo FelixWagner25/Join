7c6b046f52c6eab8161e77e0b9b5a8945e249043
Felix Wagner <post@felixwagner.eu>
2025-08-03 14:57:54 +0200

    refactor: outsource functions from contacts.js

--- scripts/contact_operations.js
+++ scripts/contact_operations.js
@@ -1,4 +1,21 @@
 /**
+ * Returns an array alphabetically sorted by name.
+ *
+ * @async
+ * @returns {Promise<Array<[string,Object]>>} A promise that resolves to an array of [id, contact] pairs sorted by name.
+ */
+async function getSortedContactsArray() {
+  let contacts = await getDataBaseElement("contacts");
+  contactsArray = Object.entries(contacts);
+  contactsArray.sort((idValuePairA, idValuePairB) => {
+    const nameA = idValuePairA[1].name.toLowerCase();
+    const nameB = idValuePairB[1].name.toLowerCase();
+    return nameA.localeCompare(nameB);
+  });
+  return contactsArray;
+}
+
+/**
  * Adds a new contact to the database.
  *
  */
@@ -69,4 +86,175 @@ async function deleteContactFromTasks(contactFirebaseId) {
     await deleteDataBaseElement(path);
   }
   tasksArray = await getTasksArray();
+}
+
+/**
+ * Sets a CSS property value of the add contact mobile button
+ *
+ * @param {string} property A CSS property
+ */
+function setVisibilityAddContactMobileBtn(property) {
+  let mobileButtonRef = document.getElementById("add-new-contact-btn-mobile");
+  mobileButtonRef.style.visibility = property;
+}
+
+/**
+ * Prefills all input fields of add contact form with current values stored on firebase server.
+ *
+ * @param {integer} indexContact
+ */
+async function prefillContactInputFields(indexContact) {
+  prefillContactInputField("name", indexContact);
+  prefillContactInputField("email", indexContact);
+  prefillContactInputField("phone", indexContact);
+}
+
+/**
+ * Prefills an input field of the add contact form with the respective current value stored on firebase server.
+ *
+ * @param {string} inputHtmlId Id of contact input html element
+ * @param {string} attributeName Contact attribute name, either "name", "email" or "phone"
+ * @param {integer} indexContact
+ */
+async function prefillContactInputField(attributeName, indexContact) {
+  let inputHtmlId = "input-" + String(indexContact) + "-" + attributeName;
+  let inputFieldRef = document.getElementById(inputHtmlId);
+  inputFieldRef.value = await getCurrentContactAttribute(
+    attributeName,
+    indexContact
+  );
+}
+
+/**
+ * Gets the current contact attribute from firebase server
+ *
+ * @param {string} attribute Contact attribute either name, email or phone
+ * @param {integer} indexContact
+ * @returns Current contact attribute
+ */
+async function getCurrentContactAttribute(attribute, indexContact) {
+  let contactId = contactsArray[indexContact][0];
+  let path = "contacts/" + contactId + "/" + attribute;
+  currentAttribute = await getDataBaseElement(path);
+  return currentAttribute;
+}
+
+/**
+ * Returns contact index inside of contactsArray by contact email.
+ *
+ * @param {string} contactEmail
+ * @returns {integer} index of contact with above email inside contactsArray
+ */
+function getContactIndexByEmail(contactEmail) {
+  let index = contactsArray.findIndex(
+    (contact) => contact[1].email === contactEmail
+  );
+  return index;
+}
+
+/**
+ * Collects a new contact's information typed into the form.
+ *
+ */
+function getContactInformation(htmlIdPrefix) {
+  let nameRef = document.getElementById(htmlIdPrefix + "name");
+  let emailRef = document.getElementById(htmlIdPrefix + "email");
+  let phoneRef = document.getElementById(htmlIdPrefix + "phone");
+  let contactData = {
+    name: nameRef.value,
+    email: emailRef.value,
+    phone: phoneRef.value,
+  };
+  clearAddContactForm(htmlIdPrefix);
+  return contactData;
+}
+
+/**
+ * Checks whether contact in contact list has a first letter predecessor. If the contact has a first letter predecessor there is no need for an additional contact list bookmark related to this contact
+ *
+ * @param {integer} indexContact
+ * @returns {boolean} true if predecessor exists
+ */
+function contactHasFirstLetterPredecessor(indexContact) {
+  if (indexContact == 0) {
+    return false;
+  } else if (
+    firstLettersAreEqual(
+      contactsArray[indexContact][1].name,
+      contactsArray[indexContact - 1][1].name
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Assigns a icon color to a contact
+ *
+ * @param {integer} indexContact
+ * @returns {string}  CSS class with color property as string
+ */
+function getContactColorClassName(indexContact) {
+  let index = indexContact % contactColorClasses.length;
+  return contactColorClasses[index];
+}
+
+/**
+ * Returns contact color class name by firebase server id
+ *
+ * @param {string} contactFirebaseId
+ * @returns {string} name of CSS class
+ */
+function getContactColorClassNameByFirebaseId(contactFirebaseId) {
+  if (!contactFirebaseId) {
+    return "";
+  } else {
+    let indexContact = findContactIndexByFirebaseId(contactFirebaseId);
+    let index = indexContact % contactColorClasses.length;
+    return contactColorClasses[index];
+  }
+}
+
+/**
+ * Returns index of contact in contactsArray by contacts firebase Id
+ *
+ * @param {string} contactFirebaseId
+ * @returns {integer} index of contact in contactsArray
+ */
+function findContactIndexByFirebaseId(contactFirebaseId) {
+  if (!contactFirebaseId) {
+    return "";
+  } else {
+    return contactsArray.findIndex(
+      (contactEntry) => contactEntry[0] === contactFirebaseId
+    );
+  }
+}
+
+/**
+ *
+ * @param {string} contactFirebaseId
+ * @returns {array} tupel of task Id and taks assigned to Id
+ */
+async function findTaskIdAssignedToIdTupels(contactFirebaseId) {
+  tasksArray = await getTasksArray();
+  let taskIdassignedToIdTupels = [];
+  for (let indexTask = 0; indexTask < tasksArray.length; indexTask++) {
+    let taskFirebaseId = tasksArray[indexTask][0];
+    let taskValueObj = tasksArray[indexTask][1];
+    if (!taskValueObj.assignedTo) continue;
+    let assignedToObj = Object.fromEntries(taskValueObj.assignedTo);
+    for (let assignedToId in assignedToObj) {
+      let contactObj = assignedToObj[assignedToId];
+      if (contactObj?.Id === contactFirebaseId) {
+        taskIdassignedToIdTupels.push({
+          taskId: taskFirebaseId,
+          assignedToId: assignedToId,
+        });
+      }
+    }
+  }
+  return taskIdassignedToIdTupels;
 }
