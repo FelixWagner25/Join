39368ddbab5ac635377d126059d52056f64422c1
Felix Wagner <post@felixwagner.eu>
2025-08-13 20:51:26 +0200

    docs: add jsdoc comments to reset_join_content.js

--- scripts/reset_join_content.js
+++ scripts/reset_join_content.js
@@ -1,28 +1,40 @@
 let initialContacts = [];
 
+/**
+ * Resets total join content
+ */
 async function resetJoinContent() {
   await deleteDataBaseElements();
   await submitInitialContacts();
   await submitInitialTasks();
-   showToastMessage("reset-join-msg")
- }
-
-async function resetContacts() {
-  await deleteDataBaseElements();
+  showToastMessage("reset-join-msg");
 }
 
+/**
+ * Deletes contacts, tasks and users from join.
+ */
 async function deleteDataBaseElements() {
   await deleteDataBaseElement("contacts");
   await deleteDataBaseElement("tasks");
   await deleteDataBaseElement("user");
 }
 
+/**
+ * Submits initial contacts to firebase server
+ */
 async function submitInitialContacts() {
   for (let i = 0; i < initialContacts.length; i++) {
     submitObjectToDatabase("contacts", initialContacts[i]);
   }
 }
 
+/**
+ * Adds initial contact to initialContacts array
+ *
+ * @param {string} name contact name
+ * @param {string} email contact email
+ * @param {string } phone contact phone
+ */
 function addInitalContact(name, email, phone) {
   initialContact = new InitialContact(name, email, phone);
   initialContacts.push(initialContact);
@@ -39,16 +51,109 @@ addInitalContact("Axel Fischer", "fischer@gmail.com", "49888888888");
 addInitalContact("Tim Weber", "weber@gmail.com", "49999999999");
 addInitalContact("Theresa Koch", "koch@gmail.com", "49101010101");
 
+/**
+ * Submits initial tasks to firebase server
+ */
 async function submitInitialTasks() {
-   let contactsRef =  await getDataBaseElement("contacts");
-   let contactsObj = Object.entries(contactsRef)
-  taskNew("technical-task","put here Description", "2025-12-12", "low", "todo","First Test Task",[{"Id":contactsObj[1][0], "name": contactsObj[1][1].name}], [{"name":"subtask1", "done": true},{"name":"subtask2", "done": false}])
-  taskNew("technical-task","responsive Reset SE?", "2025-11-12", "medium", "todo","Reset responsive iPhone SE?",[{"Id":contactsObj[2][0], "name": contactsObj[2][1].name},{"Id":contactsObj[3][0], "name": contactsObj[3][1].name}], [{"name":"subtask1", "done": true},{"name":"subtask2", "done": false}])
-  taskNew("user-story","", "2025-02-12", "urgent", "inprogress","call Boss",[], [{"name":"call Boss", "done": false},{"name":"cry", "done": false}])
-  taskNew("user-story","please add AssignedTo", "2025-08-20", "urgent", "awaitfeedback","finish Reset-Function",[{"Id":contactsObj[3][0], "name": contactsObj[3][1].name}], [{"name":"subtask1", "done": true},{"name":"subtask2", "done": true}])
-  taskNew("technical-task","", "2025-10-12", "urgent", "inprogress","El Pollo Loco!",[{"Id":contactsObj[6][0], "name": contactsObj[6][1].name}], [])
+  let contactsRef = await getDataBaseElement("contacts");
+  let contactsObj = Object.entries(contactsRef);
+  taskNew(
+    "technical-task",
+    "put here Description",
+    "2025-12-12",
+    "low",
+    "todo",
+    "First Test Task",
+    [{ Id: contactsObj[1][0], name: contactsObj[1][1].name }],
+    [
+      { name: "subtask1", done: true },
+      { name: "subtask2", done: false },
+    ]
+  );
+  taskNew(
+    "technical-task",
+    "responsive Reset SE?",
+    "2025-11-12",
+    "medium",
+    "todo",
+    "Reset responsive iPhone SE?",
+    [
+      { Id: contactsObj[2][0], name: contactsObj[2][1].name },
+      { Id: contactsObj[3][0], name: contactsObj[3][1].name },
+    ],
+    [
+      { name: "subtask1", done: true },
+      { name: "subtask2", done: false },
+    ]
+  );
+  taskNew(
+    "user-story",
+    "",
+    "2025-02-12",
+    "urgent",
+    "inprogress",
+    "call Boss",
+    [],
+    [
+      { name: "call Boss", done: false },
+      { name: "cry", done: false },
+    ]
+  );
+  taskNew(
+    "user-story",
+    "please add AssignedTo",
+    "2025-08-20",
+    "urgent",
+    "awaitfeedback",
+    "finish Reset-Function",
+    [{ Id: contactsObj[3][0], name: contactsObj[3][1].name }],
+    [
+      { name: "subtask1", done: true },
+      { name: "subtask2", done: true },
+    ]
+  );
+  taskNew(
+    "technical-task",
+    "",
+    "2025-10-12",
+    "urgent",
+    "inprogress",
+    "El Pollo Loco!",
+    [{ Id: contactsObj[6][0], name: contactsObj[6][1].name }],
+    []
+  );
 }
 
-function taskNew(category, description, dueDate, priority, status, title,assignedTo, subtasks) {
-  new Task(category, description, dueDate, priority, status, title,assignedTo, subtasks)
+/**
+ * Creates new object classed as task
+ *
+ * @param {string} category task category
+ * @param {sting} description task description
+ * @param {string} dueDate task due date
+ * @param {string} priority task priority
+ * @param {string} status task status
+ * @param {string} title task title
+ * @param {array} assignedTo task assigned contacts
+ * @param {array} subtasks task subtasks
+ */
+function taskNew(
+  category,
+  description,
+  dueDate,
+  priority,
+  status,
+  title,
+  assignedTo,
+  subtasks
+) {
+  new Task(
+    category,
+    description,
+    dueDate,
+    priority,
+    status,
+    title,
+    assignedTo,
+    subtasks
+  );
 }
